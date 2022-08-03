93a275b3fe65f1f6460c12e4654e8e4cffeb9cc7
Anne Chretien <chretien.an@gmail.com>
2022-08-03 17:29:27 +0200

    uplaod images to pinata

--- utils/upload_to_pinata.js
+++ utils/upload_to_pinata.js
@@ -1,30 +1,37 @@
-//from repo, TO BE MODIFIED
-
 const pinataSDK = require("@pinata/sdk")
 const fs = require("fs")
 const path = require("path")
+require("dotenv").config
 
-const pinataApiKey = process.env.PINATA_API_KEY || ""
-const pinataApiSecret = process.env.PINATA_API_SECRET || ""
+const pinataApiKey = process.env.PINATA_API_KEY
+const pinataApiSecret = process.env.PINATA_API_SECRET
 const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
+
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+}
 
 async function storeImages(imagesFilePath) {
     const fullImagesPath = path.resolve(imagesFilePath)
     const files = fs.readdirSync(fullImagesPath)
     let responses = []
+    console.log("Uploading to Pinata!")
     for (fileIndex in files) {
-        const readableStreamForFile = fs.createReadStream(`${fullImagesPath}/${files[fileIndex]}`)
+        const stream = fs.createReadStream(`${fullImagesPath}/${files[fileIndex]}`)
         try {
-            const response = await pinata.pinFileToIPFS(readableStreamForFile)
+            const response = await pinata.pinFileToIPFS(stream)
             responses.push(response)
         } catch (error) {
             console.log(error)
         }
     }
     return { responses, files }
+    //the responses are the hashes from IPFS
 }
 
-async function storeTokeUriMetadata(metadata) {
+async function storeMetadata(metadata) {
     try {
         const response = await pinata.pinJSONToIPFS(metadata)
         return response
@@ -34,4 +41,4 @@ async function storeTokeUriMetadata(metadata) {
     return null
 }
 
-module.exports = { storeImages, storeTokeUriMetadata }
+module.exports = { storeImages, storeMetadata }
