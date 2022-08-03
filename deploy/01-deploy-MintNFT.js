a58d6124529af685283131e5339d60bcd4d64706
Anne Chretien <chretien.an@gmail.com>
2022-08-03 15:14:54 +0200

    comments

--- deploy/01-deploy-MintNFT.js
+++ deploy/01-deploy-MintNFT.js
@@ -7,6 +7,7 @@ const { verify } = require("../utils/verify")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
 
     params = []
 
@@ -17,6 +18,8 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         waitConfirmations: network.config.waitConfirmations,
     })
 
+    // get the IPFS hashes of our tokenURIs
+
     //verifying contract on etherscan
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
         log("Verifying...")
@@ -25,3 +28,20 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 }
 
 module.exports.tags = ["all", "mintnft"]
+
+/* functions to upload to pinata and handle token URIS:
+
+let tokenUris
+
+if (process.env.UPLOAD_TO_PINATA == "true") {
+    tokenUris = await handleTokenUris()
+}
+
+async function handleTokenUris() {
+    tokenUris = []
+    => store image in IPFS then Store the metadata in IPFS
+
+    return tokenUris
+}
+
+*/
