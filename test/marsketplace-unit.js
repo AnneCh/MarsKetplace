e5caa19db0a35541aed404e208b60b9edd0bbdff
Anne Chretien <chretien.an@gmail.com>
2022-08-19 10:01:12 +0200

    boilerplate tests

--- test/marsketplace-unit.js
+++ test/marsketplace-unit.js
@@ -5,7 +5,7 @@ const { developmentChains } = require("../helper-hardhat-config")
 //skip test if not on localhost/hardhat
 !developmentChains.includes(network.name)
   ? describe.skip
-  : describe("Basic NFT minting tests", function () {
+  : describe("Unit MarsKetplace tests", function () {
       let marsketplace, deployer
       let price = ethers.utils.parseEther("1")
 
@@ -20,4 +20,19 @@ const { developmentChains } = require("../helper-hardhat-config")
         await deployments.fixture(["marsKetplace"])
         marsketplace = await ethers.getContract("MarsKetplace")
       })
+
+      describe("List item function", async () => {
+        it("Should revert if price is 0", async () => {
+          let price = ethers.utils.parseEther("0")
+          expect(
+            await marsketplace.listItem(nftaddress.address, tokenId, price)
+          ).to.be.reverted.with("MarsKetplace_PriceCantBeZero")
+        })
+        it("Should revert if the marsKetplace contract is not approved", async () => {})
+        it("Should emit an event ItemListed", async () => {})
+        it("Should only allow the owner to list an NFT", async () => {})
+        it("Should revert if the NFT is already listed", async () => {})
+      })
+
+      describe("buyItem function")
     })
