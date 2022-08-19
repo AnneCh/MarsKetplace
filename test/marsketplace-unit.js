4b4aead52105bc155a94e79add4b415366344080
Anne Chretien <chretien.an@gmail.com>
2022-08-19 11:47:43 +0200

    change test 1 NFT

--- test/marsketplace-unit.js
+++ test/marsketplace-unit.js
@@ -6,8 +6,9 @@ const { developmentChains } = require("../helper-hardhat-config")
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("Unit MarsKetplace tests", function () {
-      let marsketplace, nftDeployed, deployer, player
+      let marsketplace, oneNftDeployed, deployer, player
       let price = ethers.utils.parseEther("1")
+      const TOKENID = 0
 
       //deploys the contract and gets an instance of it to work with
       // the beforeEach() will also need to deploy the contract that will mint the NFTs,
@@ -19,18 +20,14 @@ const { developmentChains } = require("../helper-hardhat-config")
         player = accounts[1]
         await deployments.fixture(["all"])
         marsketplace = await ethers.getContract("MarsKetplace")
-        nftDeployed = await ethers.getContract("MintNFT")
+        oneNftDeployed = await ethers.getContract("MintOneToken")
+        await oneNftDeployed.approve(marsketplace.address, TOKENID)
       })
 
       describe("List item function", async () => {
-        it("Should revert if price is 0", async () => {
-          let price = ethers.utils.parseEther("0")
-          expect(
-            await marsketplace.listItem(nftaddress.address, tokenId, price)
-          ).to.be.reverted.with("MarsKetplace_PriceCantBeZero")
-        })
+        it("Should revert if price is 0", async () => {})
         it("Should revert if the marsKetplace contract is not approved", async () => {})
-        it("Should emit an event ItemListed", async () => {})
+        it("Should list and emit an event ItemListed", async () => {})
         it("Should only allow the owner to list an NFT", async () => {})
         it("Should revert if the NFT is already listed", async () => {})
       })
