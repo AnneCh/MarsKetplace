b9df0de97cf7ec95de9cdebdc230a23b55e71ff3
Anne Chretien <chretien.an@gmail.com>
2022-08-22 14:56:24 +0200

    cleanup

--- contracts/MintOneToken.sol
+++ contracts/MintOneToken.sol
@@ -6,17 +6,13 @@ pragma solidity ^0.8.7;
 
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MintOneToken is ERC721URIStorage, Ownable {
+contract MintOneToken is ERC721URIStorage {
 
     string public constant TOKEN_URI = "ipfs://QmaGmVJianGd3wzdfRNFZXWA9UeMdLFMBX5cZZSZjNeckP";
-    string internal _tokenURI; 
     uint256 private s_tokenCounter;
-    // this mapping allows to link the token ID to the corresponding index in the list of the tokenURIS
-    mapping(uint256 => string) public _IDtoURI;
 
-    event NFTMinted(uint256);
+    event NFTMinted(uint256 indexed tokenId);
     
     constructor() ERC721("Plot On Mars", "POM") {
         s_tokenCounter = 0;
