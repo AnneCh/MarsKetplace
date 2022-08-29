ba313469b4b900abf5d374e05d3ee3aec8b5bf36
Anne Chretien <chretien.an@gmail.com>
2022-08-29 09:10:50 +0200

    mint and list script

--- scripts/mint-and-list.js
+++ scripts/mint-and-list.js
@@ -4,15 +4,29 @@
 
 const { ethers } = require("hardhat")
 
+const PRICE = ethers.utils.parseEther("1")
+
 async function mintAndList() {
-    const marsKetplace = await ethers.getContract("MarsKetplace")
-    const nftMint = await ethers.getContract("MintNFT")
-    //list
+  const marsKetplace = await ethers.getContract("MarsKetplace")
+  const nftMint = await ethers.getContract("MintOneToken")
+  console.log("Minting NFT...")
+  const nftTx = await nftMint.mintNft()
+  const nftTxReceipt = await nftTx.wait(1)
+  // now from `nftTxReceipt` we can get to the events emited and grab the argument we need
+  const tokenId = nftTxReceipt.events[0].args.tokenId
+  // use `tokenId` to approve the marsKetplace contract
+  console.log("Approving MarsKetplace Contract to sell the NFT...")
+  const approvalTx = await nftMint.approve(marsKetplace.address, tokenId)
+  await approvalTx.wait(1)
+  console.log("Listing the NFT on the MarsKetplace...")
+  const listTx = await marsKetplace.listNft(nftMint.address, tokenId, PRICE)
+  await listTx.wait(1)
+  console.log("NFT successfully listed on the MarsKetplace!")
 }
 
 mintAndList()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
