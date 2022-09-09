1b4f9b19c82ac7aa635d59329fce794cea7e22b5
Anne Chretien <chretien.an@gmail.com>
2022-09-09 13:46:15 +0200

    remove unecessary

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,14 @@
-require('@nomiclabs/hardhat-waffle')
-require('@nomiclabs/hardhat-etherscan')
-require('hardhat-deploy')
-require('solidity-coverage')
-require('hardhat-gas-reporter')
-require('hardhat-contract-sizer')
-require('dotenv').config()
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-
-// const MAINNET_RPC_URL =
-//     process.env.MAINNET_RPC_URL ||
-//     process.env.ALCHEMY_MAINNET_RPC_URL
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 
@@ -22,13 +18,9 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const REPORT_GAS = process.env.REPORT_GAS || false
 
 module.exports = {
-  defaultNetwork: 'hardhat',
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      // // If you want to do some forking, uncomment this
-      // forking: {
-      //   url: MAINNET_RPC_URL
-      // }
       chainId: 31337,
     },
     localhost: {
@@ -41,12 +33,6 @@ module.exports = {
       chainId: 4,
       blockConfirmations: 8,
     },
-    // mainnet: {
-    //   url: MAINNET_RPC_URL,
-    //   accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-    //   saveDeployments: true,
-    //   chainId: 1,
-    // },
   },
   etherscan: {
     // npx hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
@@ -56,14 +42,14 @@ module.exports = {
   },
   gasReporter: {
     enabled: REPORT_GAS,
-    currency: 'USD',
-    outputFile: 'gas-report.txt',
+    currency: "USD",
+    outputFile: "gas-report.txt",
     noColors: true,
     // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
   contractSizer: {
     runOnCompile: false,
-    only: ['Raffle'],
+    only: ["Raffle"],
   },
   namedAccounts: {
     deployer: {
@@ -77,10 +63,10 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: '0.8.7',
+        version: "0.8.7",
       },
       {
-        version: '0.4.24',
+        version: "0.4.24",
       },
     ],
   },
