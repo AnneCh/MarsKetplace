6fe665c31122780b1de9574740181a55b1067775
Anne Chretien <chretien.an@gmail.com>
2022-09-13 14:35:13 +0200

    cancel script

--- scripts/cancel.js
+++ scripts/cancel.js
@@ -1,7 +1,7 @@
 const { ethers, network } = require("hardhat")
 const { moveBlocks } = require("../utils/move-blocks")
 
-const TOKEN_ID = 1
+const TOKEN_ID = 0
 
 async function cancel() {
   const marsKetplace = await ethers.getContract("MarsKetplace")
