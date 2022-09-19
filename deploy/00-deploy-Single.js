f9d0d7c9be8a7a868e2f8fd1a8d121b5293ba2cd
Anne Chretien <chretien.an@gmail.com>
2022-09-19 10:54:59 +0200

    metadata missing

--- deploy/00-deploy-Single.js
+++ deploy/00-deploy-Single.js
@@ -1,5 +1,6 @@
 const { network } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
+const { storeImages, storeMetadata } = require("../utils/upload-to-pinata")
 
 const metadataTemplate = {
   name: "",
@@ -8,7 +9,7 @@ const metadataTemplate = {
 }
 const imagesLocation = "./nftImages/"
 
-const tokenUri = "ipfs://QmXxMKvi5UgUDggqDYSUohmhgpAyZFVa7rSveyC8poYRz1"
+//const tokenUri = "ipfs://QmXxMKvi5UgUDggqDYSUohmhgpAyZFVa7rSveyC8poYRz1"
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments
@@ -24,7 +25,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     waitConfirmations: network.config.waitConfirmations,
   })
 
+  await storeImages(imagesLocation)
+
   log(`Contract deployed! the address of your contract is ${mintNFT.address}`)
+}
+
+async function handleTokenURI() {
+  tokenUri = ""
+  return tokenUri
 }
 
 module.exports.tags = ["all", "single", "main"]
--- utils/upload-to-pinata.js
+++ utils/upload-to-pinata.js
@@ -10,33 +10,33 @@ const pinataApiSecret = process.env.PINATA_API_SECRET
 const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
 
 async function storeImages(imagesFilePath) {
-    const fullImagesPath = path.resolve(imagesFilePath)
-    const files = fs.readdirSync(fullImagesPath)
-    let responses = []
-    console.log("Uploading to Pinata!")
-    for (fileIndex in files) {
-        //Pinata SDK requires a stream of the files to upload
-        const stream = fs.createReadStream(`${fullImagesPath}/${files[fileIndex]}`)
-        try {
-            const response = await pinata.pinFileToIPFS(stream)
-            responses.push(response)
-        } catch (error) {
-            console.log(error)
-        }
+  const fullImagesPath = path.resolve(imagesFilePath)
+  const files = fs.readdirSync(fullImagesPath)
+  let responses = []
+  console.log("Uploading to Pinata!")
+  for (fileIndex in files) {
+    //Pinata SDK requires a stream of the files to upload
+    const stream = fs.createReadStream(`${fullImagesPath}/${files[fileIndex]}`)
+    try {
+      const response = await pinata.pinFileToIPFS(stream)
+      responses.push(response)
+    } catch (error) {
+      console.log(error)
     }
-    console.log("Your images have been uploaded to Pinata successfully!")
-    return { responses, files }
-    //the responses are the hashes from IPFS
+  }
+  console.log("Your images have been uploaded to Pinata successfully!")
+  return { responses, files }
+  //the responses are the hashes from IPFS
 }
 
 async function storeMetadata(metadata) {
-    try {
-        const response = await pinata.pinJSONToIPFS(metadata)
-        return response
-    } catch (error) {
-        console.log(error)
-    }
-    return null
+  try {
+    const response = await pinata.pinJSONToIPFS(metadata)
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+  return null
 }
 
 module.exports = { storeImages, storeMetadata }
